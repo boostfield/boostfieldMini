515060293d0291ad2d7abb72ceba03112bb96298
mawb23 <mwbdevelop@gmail.com>
2018-03-06 18:09:19 +0800

    案例样式调整

--- service/Service.js
+++ service/Service.js
@@ -7,7 +7,7 @@ class Service {
       for (let index in data) {
         data[index] = {
           title: data[index].title.rendered,
-          subTitle: data[index].excerpt.rendered,
+          subTitle: data[index].excerpt.rendered.substring(data[index].excerpt.rendered.indexOf('<p>') + 3, data[index].excerpt.rendered.indexOf('</p>')) + '...',
           imageUrl: data[index]._embedded['wp:featuredmedia'][0].media_details.sizes.medium.source_url,
           month: new Date(data[index].date).getMonth() + 1,
           day: new Date(data[index].date).getDate(),
