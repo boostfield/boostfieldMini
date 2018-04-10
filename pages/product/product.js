fb2ffca7095fcba8ea70b37bdad57aa259031ae7
mawb23 <mwbdevelop@gmail.com>
2018-04-10 10:41:06 +0800

    官网小程序更新

--- pages/product/product.js
+++ pages/product/product.js
@@ -5,7 +5,6 @@ Page({
    * 页面的初始数据
    */
   data: {
-
     banners: [
       {
         img: "https://www.boostfield.com/wp-content/uploads/2018/03/img_home_top.jpg",
@@ -22,14 +21,15 @@ Page({
         title: "区块链全套解决方案",
         subTitle: "打造数字经济时代的信任基石"
       }
-    ]  
+    ],
+    swiperItemCss: 'flex-center'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
@@ -44,8 +44,13 @@ Page({
 
   tapQrCode() {
     wx.previewImage({
-      current: 'https://www.boostfield.com/wp-content/uploads/2018/03/img_wechat.png', // 当前显示图片的http链接
-      urls: [] // 需要预览的图片http链接列表
+      urls: ['https://www.boostfield.com/wp-content/uploads/2018/03/img_wechat.png'] // 需要预览的图片http链接列表
+    })
+  },
+
+  bindSwiperChange({ detail }) {
+    this.setData({
+      swiperItemCss: detail.current == 0 ? 'flex-center' : 'flex-left'
     })
   }
 })
